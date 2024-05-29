c116ba52b135f6456df77ced40c38f4dbd0c5adf
Ariel_EA <are3380@gmail.com>
2024-05-29 20:53:34 -0300

    Arrumando tarefas

--- portfolio/src/app/models/profile.ts
+++
@@ -1,6 +0,0 @@
-export class Profile {
-    name: string = 'Ratos da Uninove';
-    whatsAppUrl: string = 'https://api.whatsapp.com/send?phone=11994648415&text=Projeto%20Blue%20Organizer%20Seja%20bem%20vindo!';
-    linkedInUrl: string = 'https://github.com/ratosdaunilixo';
-    email: string = 'projetoblueorganizer@8962gmail.com'
-}
